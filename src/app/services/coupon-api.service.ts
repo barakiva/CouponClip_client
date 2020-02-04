494aeeec6bc55d2b12412dd430cc57766a0236ce
Bar Akiva <barakiva1@gmail.com>
2020-02-04 10:18:11 +0200

    backup

--- src/app/services/coupon-api.service.ts
+++ src/app/services/coupon-api.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CouponApiService {
+  readonly zomatoKey = "fa807880d0849e00e0765e0ee44622b0";
+  readonly zomatoBaseUrl = "https://developers.zomato.com/api/v2.1/cities?q=paris";
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getTest(){
+    // const headers: HttpHeaders = new HttpHeaders({
+    //   'Content-Type':  'application/json',
+    //   'Authorization': this.zomatoKey
+    // });
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'my-auth-token'
+      });
+
+    console.log(
+      this.http.get(this.zomatoBaseUrl, headers );
+    )
+  }
+
 }
